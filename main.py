4c3de4ebbd3bd1c7d07b22b8b095dcd362638a50
Robinkadian <59972838+Robinkadian@users.noreply.github.com>
2024-10-01 22:42:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import time
 import base64
 from PIL import Image
-import qrcode
+#import qrcode
 
 # Set wide layout for better design
 st.set_page_config(layout="wide")
@@ -114,9 +114,9 @@ def hidden_page():
     st.write("Hint: It's something you use to call for help...")
 
 # Generate QR code
-def generate_qr_code(text):
-    img = qrcode.make(text)
-    st.image(img)
+# def generate_qr_code(text):
+#     img = qrcode.make(text)
+#     st.image(img)
 
 # Dynamism: Pages show something different on each visit
 def dynamic_effects():
